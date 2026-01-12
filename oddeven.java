91454260697a2d978b53a6d6c62319f3f90383d6
kondalahari333 <kondalahari333@gmail.com>
2026-01-12 19:36:38 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,15 @@
+//even or odd number using user defined function
+import  java.util.*;
+class oddeven{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("even number.");
+        }
+        else{
+            System.out.println("odd number.");
+        }
+    }
+}
